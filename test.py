7985b2a4052cc6b524af5914ca1e1c1173c0046b
junyi1997 <q5896799@gmail.com>
2021-03-29 22:31:33 +0800

    'first_commit'

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import tkinter.messagebox
-import face_reco_from_camera_ot_single_person
-import features_extraction_to_csv
-import get_faces_from_camera    
+#import face_reco_from_camera_ot_single_person
+#import features_extraction_to_csv
+#import get_faces_from_camera    
 
 def New_person():
-    get_faces_from_camera.main()
+    #get_faces_from_camera.main()
+    pass:
 
-def update():
+def update()
     print("開始更新")
-    features_extraction_to_csv.main()
+    #features_extraction_to_csv.main()
+    pass
     print("更新成功")
 
 def fd():
-    face_reco_from_camera_ot_single_person.main()
+    #face_reco_from_camera_ot_single_person.main()
+    pass
 
 root=tk.Tk()
 root.title("人臉辨識系統")
-root.geometry("720x600+200+200")
+root.geometry("720x600+0+0")
 fontStyle = tkFont.Font(family="Lucida Grande", size=20)
 
 btn0=tk.Label(root,text="點名系統", height=5,bg='yellow',font=fontStyle).pack(side='top', fill='x')
