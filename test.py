a7263a75c1e9fd05d60eadbefcca62fba4eaa1be
junyi1997 <q5896799@gmail.com>
2021-03-30 21:01:23 +0800

    'first_commit'

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ def fd():
 
 root=tk.Tk()
 root.title("人臉辨識系統")
-root.geometry("480x300+0+0")
+root.geometry("475x300+0+0")
 #fontStyle = tkFont.Font(family="Lucida Grande", size=20)   ,font=fontStyle
 
 btn0=tk.Label(root,text="點名系統", height=3,bg='yellow',font= ('Noto Sans Mono CJK TC Regular',20)).pack(side='top', fill='x')
