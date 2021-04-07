cd0ed1ed47908ca39b7eacea3a868cebab8cf1af
junyi1997 <q5896799@gmail.com>
2021-04-07 19:49:23 +0800

    'first_commit'

--- test.py
+++ test.py
@@ -32,9 +32,9 @@ screenheight = root.winfo_screenheight()
 size = '%dx%d+%d+%d' % (width, height, (screenwidth - width)/2, (screenheight - height)/2) 
 root.geometry(size)
 btn0=tk.Label(root,text="點名系統", height=3,bg='yellow',font= ('Noto Sans Mono CJK TC Regular',20)).pack(side='top', fill='x')
-btn1=tk.Button(root,text="新增照片", width=10,bg='light green',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:New_person()).pack(side='left', fill='y')
-btn2=tk.Button(root,text="更新", width=10,bg='deep sky blue',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:update()).pack(side='left', fill='y')
-btn3=tk.Button(root,text="人臉辨識", width=10,bg='hot pink',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:fd()).pack(side='left', fill='y')
+btn1=tk.Button(root,text="新增照片", width=8,bg='light green',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:New_person()).pack(side='left', fill='y')
+btn2=tk.Button(root,text="更新", width=8,bg='deep sky blue',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:update()).pack(side='left', fill='y')
+btn3=tk.Button(root,text="人臉辨識", width=8,bg='hot pink',font= ('Noto Sans Mono CJK TC Regular',20),command=lambda:fd()).pack(side='left', fill='y')
 
 
 root.mainloop()
