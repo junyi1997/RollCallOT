5d9ee68028a04c198b0a38a1c6787bc53c514dcd
junyi1997 <q5896799@gmail.com>
2021-04-07 19:45:15 +0800

    'first_commit'

--- face_reco_from_camera_ot_single_person.py
+++ face_reco_from_camera_ot_single_person.py
@@ -72,6 +72,9 @@ class Face_Recognizer:
         self.reclassify_interval_cnt = 0
         self.reclassify_interval = 10
         self.cap=cv2.VideoCapture(0)
+        
+        self.cap.set(3, 300) # set video widht
+        self.cap.set(4, 300) # set video height
         self.close_flag=True
         #紀錄人臉框框座標
         self.img_top=0
